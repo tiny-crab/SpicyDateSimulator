f56ecd21a5258c3cbad1579a26a8f08bdcd24dda
zakattak <zachary.jay.crabtree@gmail.com>
2016-11-25 14:22:18 -0800

    Create initial project structure. Add basic main menu functionality.

---
+++ SpicyDateSimulator/app/src/main/java/com/kch/spicydatesimulator/MessagingActivity.java
@@ -0,0 +1,15 @@
+package com.kch.spicydatesimulator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MessagingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
